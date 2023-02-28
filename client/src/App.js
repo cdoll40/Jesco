390b809df26d9eb2d1c0efb96746cdd48fcdb2a5
Cody Doll <cdoll831@gmail.com>
2023-02-28 13:45:32 -0600

    Components and Pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,30 @@
 import './App.css';
+import {UserContextProvider} from "./UserContext";
+import {Route, Routes} from "react-router-dom";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import NavBar from './components/NavBar'
+import HomePage from './pages/HomePage'
+import SignIn from './pages/SignInPage';
+import Register from './pages/RegisterPage';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <UserContextProvider>
+        <NavBar />
+        <Routes>
+          <Route index element={<HomePage />} />
+          <Route path='/SignIn' element={<SignIn />} />
+          <Route path='/Register' element={<Register />} />
+        </Routes>
+      </UserContextProvider>
+    </ThemeProvider>
   );
 }
 
