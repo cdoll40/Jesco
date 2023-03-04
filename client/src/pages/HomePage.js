0ead4dc7a505951182868f33e5228b297b039de3
Cody Doll <cdoll831@gmail.com>
2023-03-04 10:55:39 -0600

    Post

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -2,10 +2,6 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Button from '@mui/material/Button';
 import CameraIcon from '@mui/icons-material/PhotoCamera';
-// import Card from '@mui/material/Card';
-// import CardActions from '@mui/material/CardActions';
-// import CardContent from '@mui/material/CardContent';
-// import CardMedia from '@mui/material/CardMedia';
 import CssBaseline from '@mui/material/CssBaseline';
 import Grid from '@mui/material/Grid';
 import Stack from '@mui/material/Stack';
@@ -77,7 +73,9 @@ export default function Album() {
           <Grid container spacing={4}>
             {cards.map((card) => (
               <Grid item key={card} xs={12} sm={6} md={4}>
+                
                 <Post />
+                
                 {/* <Card
                   sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
                 >
